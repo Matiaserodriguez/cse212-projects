b2b3691dca6db2b654bfa237573d0ebe7000f014
Matias Rodriguez <maturod96@gmail.com>
2024-03-09 19:55:48 -0300

    Fixes the PriorityQueue

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -8,22 +8,39 @@
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: The Dequeue function shall remove the item with the highest priority and return its value.
+        // Expected Result: The item dequeued was: banana & Queue: [Apple (Pri:1), orange (Pri:1)]
         Console.WriteLine("Test 1");
+        priorityQueue.Enqueue("Apple", 1);
+        priorityQueue.Enqueue("banana", 2);
+        priorityQueue.Enqueue("orange", 1);
+        
+        var priorityItem = priorityQueue.Dequeue();
+        Console.WriteLine("The item dequeued was: " + priorityItem);
+        Console.WriteLine("Queue: " + priorityQueue);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: The .Dequeue() isn't dequeuing
 
         Console.WriteLine("---------");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: If there are more than one item with the highest priority, then the item closest to the front of the queue will be removed and its value returned.
+        // Expected Result: Queue: [orange (Pri:1), watermelon (Pri:1)]
         Console.WriteLine("Test 2");
+        priorityQueue.Enqueue("watermelon", 1);
+        priorityQueue.Dequeue();
+        Console.WriteLine("Queue: " + priorityQueue);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: The FIFO isn't working as expected
 
         Console.WriteLine("---------");
+        // Test 3
+        // Scenario: If the queue is empty, then an error message will be displayed.
+        // Expected Result: "The queue is empty."
+        Console.WriteLine("Test 3");
+        priorityQueue.Dequeue();
+        priorityQueue.Dequeue();
+        priorityQueue.Dequeue();
 
         // Add more Test Cases As Needed Below
     }
--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -13,7 +13,7 @@
         _queue.Add(newNode);
     }
 
-    public String Dequeue() {
+    public string Dequeue() {
         if (_queue.Count == 0) // Verify the queue is not empty
         {
             Console.WriteLine("The queue is empty.");
@@ -22,13 +22,14 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++) {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+        for (int index = 0; index < _queue.Count - 1; index++) {
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
