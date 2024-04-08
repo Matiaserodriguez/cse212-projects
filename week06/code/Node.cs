5a72a2214368e33ef8ebb64dc32fe53acb2d471a
Matias Rodriguez <maturod96@gmail.com>
2024-04-08 20:09:46 -0300

    Solves problems for week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,25 +12,49 @@ public class Node {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
-            else
+            else if (Left.Data != value)
                 Left.Insert(value);
         }
         else {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
-            else
+            else  if (Right.Data != value)
                 Right.Insert(value);
         }
     }
 
     public bool Contains(int value) {
-        // TODO Start Problem 2
+        
+        if (value < Data) {
+            if (Left != null)
+                if (value == Left.Data)
+                    return true;
+                else if (Left.Contains(value))
+                    return true;
+        }
+        else {
+            if (Right != null)
+                if (value == Right.Data)
+                    return true;
+                else if (Right.Contains(value)) 
+                    return true;            
+        }
         return false;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int counterLeft = 1;
+        int counterRight = 1;
+        if (Left != null)
+            counterLeft += Left.GetHeight();
+        if (Right != null)
+            counterRight += Right.GetHeight();
+        
+        if (counterLeft > counterRight)
+            return counterLeft;
+        
+        return counterRight; // Replace this line with the correct return statement(s)
     }
 }
